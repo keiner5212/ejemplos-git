d1a26810d113d546f0befc023011bd47c40afed0
Daniel Calderon <daniel.calderon@falconfarms.com>
2026-05-07 11:02:07 -0400

    Version 8

--- variables.py
+++ variables.py
@@ -4,3 +4,10 @@ edad = 21 # int
 
 # aqui imprimiremos el nombre
 print("Hola",nombre, "tienes: ", edad, "años")
+
+# aqui vamos a definir el nombre
+nombre = "Daniel" # str
+edad = 25 # int
+
+# aqui imprimiremos el nombre
+print("Hola",nombre, "tienes: ", edad, "años")
